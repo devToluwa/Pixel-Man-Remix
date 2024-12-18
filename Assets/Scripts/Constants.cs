10c4e7f0443bf4cb1a32179d46768e3024ddaaee
devToluwa <toluwadev@outlook.com>
2024-12-18 14:01:37 +0100

    Added Run animation
    Added running animations
    Need to fix jump issue, currently jump logic is not working as intended

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -6,7 +6,9 @@ public static class Constants
   public static readonly string GROUND_TAG = "Ground";
 
   // Names of Animation parameters
-  public static readonly string IS_RUNNING_ANIM_TAG = "IsRunning";
-  public static readonly string IS_RUNNING_FLOAT_ANIM_TAG = "isRunningFloat";
-  public static readonly string IS_JUMPING_ANIM_TAG = "IsJumping";
+  public static readonly string IS_RUNNING_ANIM_STRING = "IsRunning";
+  public static readonly string JUMP_ANIM_TRIGGER_STRING = "Jump";
+  public static readonly string IS_GROUNDED_ANIM_STRING = "IsGrounded";
+  public static readonly string IS_FALLING_ANIM_STRING = "IsFalling";
+  public static readonly string Y_VELOCITY_ANIM_STRING = "yVelocity";
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
     [SerializeField] float jumpPower;
+    [SerializeField] float groundContactCount = 0;
 
     private bool isGrounded;
 
@@ -14,7 +16,7 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
@@ -26,6 +28,7 @@ public class Player : MonoBehaviour
     {
         HandlePlayerMovement();
         HandleJumping();
+
         HandleFlipping();
         HandleAnimations();
     }
@@ -39,20 +42,17 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             moveInput = moveSpeed;
-            // FIX MOVEMENT ANIMTIONS
-            //animator.SetBool(Constants.IS_RUNNING_ANIM_TAG, true);
+            animator.SetBool(Constants.IS_RUNNING_ANIM_STRING, true);
         }
         else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             moveInput = -moveSpeed;
-            // FIX MOVEMENT ANIMTIONS
-            //animator.SetBool(Constants.IS_RUNNING_ANIM_TAG, true);
+            animator.SetBool(Constants.IS_RUNNING_ANIM_STRING, true);
         }
         else
         {
             moveInput = 0f;
-            // FIX MOVEMENT ANIMTIONS
-            //animator.SetBool(Constants.IS_RUNNING_ANIM_TAG, false);
+            animator.SetBool(Constants.IS_RUNNING_ANIM_STRING, false);
         }
 
         rb.linearVelocity = new Vector2(moveInput, rb.linearVelocity.y);
@@ -60,45 +60,50 @@ public class Player : MonoBehaviour
 
     private void HandleJumping()
     {
-        if (isGrounded)
+        if (isGrounded && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)))
         {
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                Debug.Log("We hit the ground and are pressing space or up arrow");
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
-                isGrounded = false;
-                // animator.SetTrigger(Constants.IS_JUMPING_ANIM_TAG);
-            }
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+            isGrounded = false;
         }
     }
 
     private void HandleAnimations()
     {
-        // FIX MOVEMENT ANIMTIONS
-        //animator.SetFloat(Constants.IS_RUNNING_FLOAT_ANIM_TAG, rb.linearVelocityX);
+        animator.SetFloat(Constants.Y_VELOCITY_ANIM_STRING, rb.linearVelocity.y);
+        animator.SetBool(Constants.IS_GROUNDED_ANIM_STRING, isGrounded);
     }
 
     private void HandleFlipping()
     {
         if (rb.linearVelocity.x > 0)
         {
-            Debug.Log("Facing Right");
             transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x), transform.localScale.y);
         }
         else if (rb.linearVelocity.x < 0)
         {
-            Debug.Log("Facing Left");
             transform.localScale = new Vector2(-Mathf.Abs(transform.localScale.x), transform.localScale.y);
         }
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag(Constants.GROUND_TAG))
+        if (other.gameObject.CompareTag(Constants.GROUND_TAG) && rb.linearVelocity.y <= 0)
         {
+            groundContactCount++;
             isGrounded = true;
-            HandleJumping();
-            Debug.Log("Hit the ground");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag(Constants.GROUND_TAG))
+        {
+            groundContactCount--;
+            if (groundContactCount <= 0)
+            {
+                isGrounded = false;
+                groundContactCount = 0;
+            }
         }
     }
 
